24baa22e2819eb67b48ac1ecd6652c5fe0854251
Juan A. Rubio <jarubio2001@gmail.com>
2018-03-09 16:40:39 +0000

    snapcraft.yaml: some more work on enabling d-bus daemons (still work-in-progress)

--- wrapper/tizonia-snap-wrapper.sh
+++ wrapper/tizonia-snap-wrapper.sh
@@ -17,6 +17,6 @@ esac
 export XDG_CONFIG_DIRS=$SNAP/etc/xdg:$XDG_CONFIG_DIRS
 
 # Tizonia's plugins directory
-export TIZONIA_PLUGINS_DIR=$SNAP/lib/tizonia0-plugins12
+export TIZONIA_PLUGINS_DIR=$SNAP/usr/lib/tizonia0-plugins12
 
-$SNAP/bin/tizonia "$@"
+$SNAP/usr/bin/tizonia "$@"
