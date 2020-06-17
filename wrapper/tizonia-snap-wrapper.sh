26afdf62dff41ffc76367d3cbc26c2fa7c9783a1
Juan A. Rubio <jarubio2001@gmail.com>
2020-06-17 12:25:19 +0100

    wrapper/tizonia-snap-wrapper.sh: re-added i386 and armhf archs. Also fixed the plugins path.

--- wrapper/tizonia-snap-wrapper.sh
+++ wrapper/tizonia-snap-wrapper.sh
@@ -6,10 +6,10 @@ export LC_ALL
 case "$SNAP_ARCH" in
     "amd64") ARCH='x86_64-linux-gnu'
              ;;
-#     "i386") ARCH='i386-linux-gnu'
-#             ;;
-#     "armhf") ARCH="arm-linux-gnueabihf"
-#              ;;
+    "i386") ARCH='i386-linux-gnu'
+            ;;
+    "armhf") ARCH="arm-linux-gnueabihf"
+             ;;
     *)
         echo "Unsupported architecture for this app build"
         exit 1
@@ -20,6 +20,6 @@ esac
 export XDG_CONFIG_DIRS=$SNAP/etc/xdg:$XDG_CONFIG_DIRS
 
 # Tizonia's plugins directory
-export TIZONIA_PLUGINS_DIR=$SNAP/usr/lib/x86_64-linux-gnu/tizonia0-plugins12
+export TIZONIA_PLUGINS_DIR=$SNAP/usr/lib/$SNAP_ARCH/tizonia0-plugins12
 
 $SNAP/usr/bin/tizonia "$@"
